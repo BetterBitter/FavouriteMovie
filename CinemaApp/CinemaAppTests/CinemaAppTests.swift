c27e3f9f5a471f9e7960a837575eee691e6d2c8e
Riko Laimena <30501260+BetterBitter@users.noreply.github.com>
2017-09-28 16:17:40 +1000

    Add files via upload

---
+++ CinemaApp/CinemaAppTests/CinemaAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  CinemaAppTests.swift
+//  CinemaAppTests
+//
+//  Created by Riko Pratama Laimena on 9/19/17.
+//  Copyright © 2017 Riko Pratama Laimena. All rights reserved.
+//
+
+import XCTest
+@testable import CinemaApp
+
+class CinemaAppTests: XCTestCase {
+    
+}
